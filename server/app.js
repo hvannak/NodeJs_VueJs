53f6bce98ee7ec0a8d270888a74342bd20d2e306
hvannak <vannak2010@gmail.com>
2020-11-10 16:00:42 +0700

    routes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { post } = require('./routes/posts');
+require('dotenv/config');
+//Import Routes
+const postsRoute = require('./routes/posts');
 
-app.get("/api",(req,res) => {
+app.use('/posts',postsRoute);
+
+app.get('/',(req,res) => {
     res.json({
         success: 1,
         message: "This is rest api working"
     });
 });
 
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true},()=>{
+    console.log('connected to DB')
+});
+
 app.listen(3000,()=> {
     console.log("Server up and running");
-})
+});
