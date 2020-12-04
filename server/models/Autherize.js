b048aa335775e4d469ac129e4638117721ab6e36
hvannak <vannak2010@gmail.com>
2020-12-04 16:58:50 +0700

    thinking autherize schema

--- server/models/Autherize.js
+++ server/models/Autherize.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
 const routerSchema = new mongoose.Schema({ 
-    method: {
-      type:String,
-      require:true
-    }, 
-    path: {
+    routes:[{
+        method: {
         type:String,
         require:true
-    }
+        }, 
+        path: {
+            type:String,
+            require:true
+        }
+    }]
 });
+
+const propsSchema = new mongoose.Schema({
+    props:[String]
+})
 
 const autherizeSchema = mongoose.Schema({
     role:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }],  
-    props:[String],
+    props:[propsSchema],
     router:[routerSchema],
     date:{
         type:Date,
