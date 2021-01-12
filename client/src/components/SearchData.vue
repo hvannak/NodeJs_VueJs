b72fcaec942da1da83435bf32bf6e46167aaebb1
vannak heng <vannak2010@gmail.com>
2021-01-12 21:14:16 +0700

    image buffer

--- client/src/components/SearchData.vue
+++ client/src/components/SearchData.vue
@@ -10,11 +10,7 @@
       hide-default-footer
     >
       <template v-slot:header>
-        <v-toolbar
-          dark
-          color="blue darken-3"
-          class="mb-1"
-        >
+        <v-toolbar dark color="blue darken-3" class="mb-1">
           <template v-if="$vuetify.breakpoint.mdAndUp">
             <v-select
               v-model="sortBy"
@@ -25,24 +21,11 @@
               prepend-inner-icon="mdi-magnify"
               label="Sort by"
             ></v-select>
-            <v-btn-toggle class="mx-2"
-              v-model="sortDesc"
-              mandatory
-            >
-              <v-btn
-                large
-                depressed
-                color="blue"
-                :value="false"
-              >
+            <v-btn-toggle class="mx-2" v-model="sortDesc" mandatory>
+              <v-btn large depressed color="blue" :value="false">
                 <v-icon>mdi-arrow-up</v-icon>
               </v-btn>
-              <v-btn
-                large
-                depressed
-                color="blue"
-                :value="true"
-              >
+              <v-btn large depressed color="blue" :value="true">
                 <v-icon>mdi-arrow-down</v-icon>
               </v-btn>
             </v-btn-toggle>
@@ -58,100 +41,64 @@
             cols="12"
             sm="6"
             md="4"
-            lg="3"
+            lg="4"
           >
-            <!-- <v-card>
-              <v-card-title class="subheading font-weight-bold">
-                {{ item.title }}
-              </v-card-title>
+            <v-card class="mx-auto my-2">
+              <v-img height="250" :src="readBufferImg(item.image[0])"></v-img>
 
-              <v-divider></v-divider>
+              <v-card-title>{{ item.title }}</v-card-title>
 
-              <v-list dense>
-                
-              </v-list>
-            </v-card> -->
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating
+                    :value="4.5"
+                    color="amber"
+                    dense
+                    half-increments
+                    readonly
+                    size="14"
+                  ></v-rating>
 
-        <v-card
-            class="mx-auto my-2"
-        >
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
 
-            <!-- <v-img
-            height="250"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img> -->
+                <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
 
-            <!-- {{item.image[0].data}} -->
-            <v-img height="250" :src="readBufferImg(item.image[0])"></v-img>
-
-            <v-card-title>{{ item.title }}</v-card-title>
-
-            <v-card-text>
-            <v-row
-                align="center"
-                class="mx-0"
-            >
-                <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-                ></v-rating>
-
-                <div class="grey--text ml-4">
-                4.5 (413)
+                <div>
+                  Small plates, salads & sandwiches - an intimate setting with
+                  12 indoor seats plus patio seating.
                 </div>
-            </v-row>
+              </v-card-text>
 
-            <div class="my-4 subtitle-1">
-                $ • Italian, Cafe
-            </div>
+              <v-divider class="mx-4"></v-divider>
 
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-            </v-card-text>
+              <v-card-title>Tonight's availability</v-card-title>
 
-            <v-divider class="mx-4"></v-divider>
+              <v-card-text> </v-card-text>
 
-            <v-card-title>Tonight's availability</v-card-title>
-
-            <v-card-text>
-
-            </v-card-text>
-
-            <v-card-actions>
-            <v-btn
-                color="deep-purple lighten-2"
-                text
-            >
-                VIEW
-            </v-btn>
-            </v-card-actions>
-        </v-card>
-
-
-
-
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text> VIEW </v-btn>
+              </v-card-actions>
+            </v-card>
           </v-col>
         </v-row>
       </template>
 
       <template v-slot:footer>
         <div class="text-center">
-            <v-container>
+          <v-container>
             <v-row justify="center">
-                <v-col cols="8">
+              <v-col cols="8">
                 <v-container class="max-width">
-                    <v-pagination
+                  <v-pagination
                     v-model="page"
                     class="my-4"
                     :length="numberOfPages"
-                    ></v-pagination>
+                  ></v-pagination>
                 </v-container>
-                </v-col>
+              </v-col>
             </v-row>
-            </v-container>
+          </v-container>
         </div>
       </template>
     </v-data-iterator>
@@ -160,39 +107,42 @@
 <script>
 import { mapGetters } from "vuex";
 
-  export default {
-    data () {
-      return {
-        search: '',
-        filter: {},
-        sortDesc: false,
-        page: 1,
-        itemsPerPage: 20,
-        sortBy: 'name',
-        keys: [
-          'Name',
-          'Calories',
-          'Fat',
-          'Carbs',
-          'Protein',
-          'Sodium',
-          'Calcium',
-          'Iron',
-        ],
-      }
+export default {
+  data() {
+    return {
+      search: "",
+      filter: {},
+      sortDesc: false,
+      page: 1,
+      itemsPerPage: 20,
+      sortBy: "name",
+      keys: [
+        "Name",
+        "Calories",
+        "Fat",
+        "Carbs",
+        "Protein",
+        "Sodium",
+        "Calcium",
+        "Iron",
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["allPosts", "getPosttotalItems"]),
+    numberOfPages() {
+      return this.getPosttotalItems < 20 ? 0 : this.getPosttotalItems / 20;
     },
-    computed: {
-      ...mapGetters(["allPosts","getPosttotalItems"]),
-      numberOfPages () {
-        return (this.getPosttotalItems < 20) ? 0 : (this.getPosttotalItems / 20);
-      },
+  },
+  methods: {
+    readBufferImg(image) {
+      var bytes = new Uint8Array(image.data);
+      var binary = bytes.reduce(
+        (data, b) => (data += String.fromCharCode(b)),
+        ""
+      );
+      return binary;
     },
-    methods: {
-        readBufferImg(image){
-           let thum = new Blob([image.data]);
-           console.log('test' + thum);
-           return thum;
-        }
-    },
-  }
+  },
+};
 </script>
