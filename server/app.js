b1caf0c9140b6c406f14591c7dd86c214ec67dd1
hvannak <vannak2010@gmail.com>
2021-03-05 17:04:54 +0700

    fallback history npm build router

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require("helmet");
 const path = require('path');
 const compression = require('compression');
+const history = require('connect-history-api-fallback');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -23,7 +24,6 @@ require('dotenv/config');
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.json());
 app.use(express.json({limit: '16mb'}));
 app.use(express.urlencoded({limit: '16mb'}));
@@ -45,6 +45,8 @@ app.use('/api/autherize',autherizeRoute);
 app.use('/api/category',categoryRoute);
 app.use('/api/language',languageRoute);
 app.use('/api/localization',localizationRoute);
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/router',verify,(req,res) => {
     try {
